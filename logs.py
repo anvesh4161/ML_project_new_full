3bd6fcf38d7c6b82aee750e7b51d52abd9aa6647
Anvesh Reddy <anveshbunty01@gmail.com>
2023-12-04 19:29:14 +0530

    Logger and Exception

--- logs.py
+++ logs.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def index():
     logging.info("We are testing our logging file")
 
-    return "Welcome to Engineering Wala Bhiya"
+    return "Welcome to ML Project"
 
 if __name__ == "__main__":
     app.run(debug = True) # 5000
